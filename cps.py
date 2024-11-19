07157946708028b0e47249bc3cb0e9c67799d73a
N122-ru <150384212+N122-ru@users.noreply.github.com>
2024-11-19 21:42:47 +0400

    Update cps.py

--- cps.py
+++ cps.py
@@ -1,7 +1,7 @@
 import subprocess
 
 try:
-    result = subprocess.run(['cps', '-p', '/config/app.db', '-g', '/config/gd.db'], check=True, text=True, capture_output=True)
+    result = subprocess.run(['/app/calibre-web/bin/cps', '-p', '/config/app.db', '-g', '/config/gd.db'], check=True, text=True, capture_output=True)
 except FileNotFoundError:
     print("cps not found")
 except subprocess.CalledProcessError as e:
